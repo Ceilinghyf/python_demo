93ce9f01c87f4141dee87a65f36836df5c24f0b2
Ceilinghyf <1415575216@qq.com>
2026-09-20 15:53:49 +0800

    测试 完成，恢复初始化

--- cnn_sign/Google Colab.py
+++ cnn_sign/Google Colab.py
@@ -86,10 +86,6 @@ model = tf.keras.Sequential([
     tf.keras.layers.Flatten(),# Flatten展平层：把二维的特征图压扁成一维向量，送给后面全连接层
     tf.keras.layers.Dense(units=6, activation="softmax") # softmax 激活函数会把这 6 个数字经过指数运算和归一化，变成 6 个 0 到 1 之间的概率值，并且所有概率加起来等于 1。
     #二分类用sigmoid激活函数，输出0~1之间的概率值；多分类用softmax激活函数，输出每个类别的概率值，所有类别概率加起来等于1。
-
-    #第二次测试提交和推送
-    #第三次测试提交和推送
-    #现在开始测试远程更新，测试vscode的同步功能
 ])
 
 # ===================== 3.编译模型 =====================
@@ -131,4 +127,3 @@ plt.title("Loss")
 plt.xlabel("epoch")
 plt.legend()
 plt.show()   
-#11
